0125ce5b2003869345f04335bdf5fb17d3c82f34
gongahkia <angryapplegravy@gmail.com>
2023-03-17 15:38:00 +0800

    oh right

--- src/Texteditor.java
+++ src/Texteditor.java
@@ -26,7 +26,8 @@ public class Texteditor extends JFrame implements KeyListener { // taking JFrame
         this.add(writingArea);
 
         this.setVisible(true);
-        
+// * ADD A TEXT LABEL AREA THAT UPDATES THE MODE OF THE EDITOR BASED ON THE CASE NUMBER
+// * ADD A TEXT LABEL AREA THAT SHOWS CURRENT COMMAND ENTERED DURING NORMAL MODE
     }
 
 // --- methods implemented from the KeyListener interface 
@@ -37,21 +38,65 @@ public class Texteditor extends JFrame implements KeyListener { // taking JFrame
         // --- to check editors current mode
         switch(editorMode) {
 
-            case 0: // 0: normal mode
+            case 0: // 0: normal mode --> (navigation with cursor), (entering insert mode), (enter command mode)
                 System.out.println("normal mode");
+                // --- 'i', 'I', 'a', 'A', 'o', 'O' bring you to insert mode
+                if (e.getKeyChar() == 'i') {
+                    editorMode = 1;
+                } else if (e.getKeyChar() == 'a') {
+                    editorMode = 1;
+                } else if (e.getKeyChar() == 'o') {
+                    editorMode = 1;
+                } else if (e.getKeyChar() == 'I') {
+                    editorMode = 1;
+                } else if (e.getKeyChar() == 'A') {
+                    editorMode = 1;
+                } else if (e.getKeyChar() == 'O') {
+                    editorMode = 1;
+// * IMPLEMENT LOGIC FOR THE ABOVE POSITION OF THE CURSOR
+                // --- 'k', 'j', 'w' and 'b' move cursor up and down, one word forward and one word back
+                } else if (e.getKeyChar() == 'j') {
+                    
+                } else if (e.getKeyChar() == 'k') {
+
+                } else if (e.getKeyChar() == 'w') {
+
+                } else if (e.getKeyChar() == 'b') { 
+// * IMPLEMENT LOGIC FOR THIS ABOVE PORTION REGARDING CURSOR NAVIGATION (no change in mode)
+                // --- 'u' undos the previous change
+                } else if (e.getKeyChar() == 'u') {
+                    System.out.println("Change undone");
+// * IMPLEMENT LOGIC FOR THE ABOVE UNDO COMMAND 
+                // --- ':' brings you to command mode
+                } else if (e.getKeyChar() == ':') {
+                    editorMode = 2;
+                } else {};
                 break;
 
-            case 1: // 1: insert mode
-                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) { // escape key brings you to normal mode
+            case 1: // 1: insert mode --> (typing)
+                System.out.println("insert mode");
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) { // --- escape key brings you to normal mode
                     editorMode = 0;
                 } else {
                     System.out.println(e.getKeyChar());
                 }
-                System.out.println("insert mode");
+// * DO I NEED TO IMPLEMENT LOGIC FOR DELETING CHARACTERS IN THIS, OR DOES JTEXTAREA DO IT FOR ME?`
                 break;
 
             case 2: // 2: command mode
                 System.out.println("command mode");
+                if (e.getKeyChar() == 'w') {
+                    System.out.println("Changes written (saved)"); 
+                    editorMode = 0;
+// * IMPLEMENT LOGIC TO SAVE CHANGES TO FILE
+                } else if (e.getKeyChar() == 'q') {
+                    System.out.println("Quitting now. Make sure you have saved your changes!");
+                    editorMode = 0;
+// * IMPLEMENT LOGIC TO QUIT PROGRAM 
+                } else {
+                    System.out.println("Command not recognised");
+                    editorMode = 0;
+                };
                 break;
 
             default:

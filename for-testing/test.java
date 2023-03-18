cf0c56cc2108182de8cdba83a215caab0edff8cd
gongahkia <angryapplegravy@gmail.com>
2023-03-18 13:10:22 +0800

    new edits

--- for-testing/test.java
+++ for-testing/test.java
@@ -7,11 +7,26 @@ import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import java.awt.Color;
 
+// --- opening a file
+import javax.swing.JFileChooser;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+// --- writing changes to a file
+
 public class test extends JFrame implements KeyListener{
 
     public int counter = 0;
     JLabel label1; // --- declare this variable first and then reference it later in my other overridden method!!!
 
+// --- opening a file
+    String currLine;
+    BufferedReader bufReader;
+
+// --- same constructor statement
     test() {
         this.setTitle("test");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,6 +37,32 @@ public class test extends JFrame implements KeyListener{
         text1.setBounds(0,0,600,500);
         text1.addKeyListener(this);
 
+// --- TO OPEN AND DISPLAY AN EDITABLE TEXT FILE IN THE TEXT AREA
+        JFileChooser fileSelector = new JFileChooser();
+        fileSelector.setCurrentDirectory(new File(System.getProperty("user.home")));
+        int result = fileSelector.showOpenDialog(label1);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            System.out.println("done");
+            File targetFile = fileSelector.getSelectedFile();
+
+            try {
+
+                bufReader = new BufferedReader(new FileReader(targetFile));
+                text1.read(bufReader, null);
+                bufReader.close();
+                text1.requestFocus();
+                // --- alternative implementation
+                /*** while ((currLine = bufReader.readLine()) != null) {
+                    System.out.println(currLine); ***/
+            }
+
+            catch (Exception e2) {
+                System.out.println(e2);
+            }
+            
+        }
+
         label1 = new JLabel();
         label1.setBackground(Color.lightGray);
         label1.setBounds(0,500,600,30);
@@ -31,8 +72,12 @@ public class test extends JFrame implements KeyListener{
         this.add(label1);
 
         this.setVisible(true);
+
+
     };
     
+// --- 
+
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyChar() == 'y') {

ecb18c2c171fe73e74e19bbf923cd4f7de5d320e
gongahkia <angryapplegravy@gmail.com>
2026-09-05 13:24:20 +0800

    newthingstoworkonandidea

--- src/test/java/shed/ReferenceDocumentHighlightLanguageServer.java
+++ src/test/java/shed/ReferenceDocumentHighlightLanguageServer.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
